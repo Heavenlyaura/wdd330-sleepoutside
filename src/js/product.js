edea56640c25179d3f7b9c6e457bcea0ff56709b
kmkryz <kmkryz@gmail.com>
2024-06-18 13:03:52 -0400

    product.js update

--- src/js/product.js
+++ src/js/product.js
@@ -4,15 +4,19 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let currentCart = getLocalStorage("so-cart");
+  // let currentCart = getLocalStorage("so-cart");
 
-  if (!currentCart || !Array.isArray(currentCart)) {
-    currentCart = [];
-  }
+  // if (!currentCart || !Array.isArray(currentCart)) {
+  //   currentCart = [];
+  // }
   
 
-  currentCart.push(product);
-  setLocalStorage("so-cart", currentCart);
+  // currentCart.push(product);
+  // setLocalStorage("so-cart", currentCart);
+
+  let cart = getLocalStorage("so-cart") || [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 
 async function addToCartHandler(e) {
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -11,9 +10,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html",
-        ),
-
+        product: resolve(__dirname, "src/product_pages/index.html")
       },
     },
   },
