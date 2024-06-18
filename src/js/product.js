b8271a31d0f88ac60439dac2ca68509db29e95ec
kmkryz <kmkryz@gmail.com>
2024-06-18 13:04:45 -0400

    product.js

--- src/js/product.js
+++ src/js/product.js
@@ -4,16 +4,6 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  // let currentCart = getLocalStorage("so-cart");
-
-  // if (!currentCart || !Array.isArray(currentCart)) {
-  //   currentCart = [];
-  // }
-  
-
-  // currentCart.push(product);
-  // setLocalStorage("so-cart", currentCart);
-
   let cart = getLocalStorage("so-cart") || [];
   cart.push(product);
   setLocalStorage("so-cart", cart);
