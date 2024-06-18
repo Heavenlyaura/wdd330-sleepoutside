05b0ddf8388be45273e57731ac6b23d59008e70e
kmkryz <kmkryz@gmail.com>
2024-06-18 12:26:20 -0400

    cart total

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,64 @@
-import { getLocalStorage } from "./utils.mjs";
+// import { getLocalStorage } from "./utils.mjs";
+
+// function renderCartContents() {
+//   const cartItems = getLocalStorage("so-cart");
+//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+// }
+
+// function cartItemTemplate(item) {
+//   const newItem = `<li class="cart-card divider">
+//   <a href="#" class="cart-card__image">
+//     <img
+//       src="${item.Image}"
+//       alt="${item.Name}"
+//     />
+//   </a>
+//   <a href="#">
+//     <h2 class="card__name">${item.Name}</h2>
+//   </a>
+//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+//   <p class="cart-card__quantity">qty: 1</p>
+//   <p class="cart-card__price">$${item.FinalPrice}</p>
+// </li>`;
+
+//   return newItem;
+// }
+
+// renderCartContents();
+
+
+// cart.js
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const cartItems = getLocalStorage("so-cart") || [];
+  renderListWithTemplate(cartItemTemplate, document.querySelector(".product-list"), cartItems);
+
+  if (cartItems.length > 0) {
+    document.querySelector(".cart-footer").classList.remove("hide");
+    const total = calculateTotal(cartItems);
+    document.querySelector(".cart-total").innerHTML = `Total: $${total.toFixed(2)}`;
+  }
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  return `
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img src="${item.Image}" alt="${item.Name}" />
+      </a>
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__quantity">qty: 1</p>
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+    </li>`;
+}
 
-  return newItem;
+function calculateTotal(cartItems) {
+  return cartItems.reduce((total, item) => total + item.FinalPrice, 0);
 }
 
 renderCartContents();
