12c108a16e4fa195e0cbfb57519a1ae87ecaf21f
kmkryz <kmkryz@gmail.com>
2024-06-18 14:29:50 -0400

    fixed for double quotes

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,6 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const listElement = document.querySelector('#product-list');
-const productListing = new ProductListing('tents', dataSource, listElement);
+const listElement = document.querySelector("#product-list");
+const productListing = new ProductListing("tents", dataSource, listElement);
 productListing.init();
