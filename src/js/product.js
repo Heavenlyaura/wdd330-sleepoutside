4953ab5572bd4fa4091faeec4edf6b1924066d21
kmkryz <kmkryz@gmail.com>
2024-06-19 11:50:10 -0400

    team activity + stretch

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,14 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs"
+
 
 const dataSource = new ProductData("tents");
+const productId = getParams("product");
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   let cart = getLocalStorage("so-cart") || [];
