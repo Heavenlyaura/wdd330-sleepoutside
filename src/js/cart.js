b1ffb24079d574504a3a93f804008cf8f35aae0f
kmkryz <kmkryz@gmail.com>
2024-06-21 16:57:29 -0400

    cart total activity - kk

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,40 +1,33 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const price = document.querySelector(".cart-total")
-  if (cartItems) {
-    price.style.display = "block"
-    let totalPrice = 0
-    cartItems.forEach(item => {
-      let itemPrice = parseInt(item.FinalPrice)
-      totalPrice += itemPrice
+  const cartItems = getLocalStorage("so-cart") || [];
+  renderListWithTemplate(cartItemTemplate, document.querySelector(".product-list"), cartItems);
 
-    });
-    price.textContent = `Total: $${totalPrice}`
+  if (cartItems.length > 0) {
+    document.querySelector(".cart-footer").classList.remove("hide");
+    const total = calculateTotal(cartItems);
+    document.querySelector(".cart-total").innerHTML = `Total: $${total.toFixed(2)}`;
   }
-
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  return `
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img src="${item.Image}" alt="${item.Name}" />
+      </a>
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__quantity">qty: 1</p>
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+    </li>`;
+}
 
-  return newItem;
+function calculateTotal(cartItems) {
+  return cartItems.reduce((total, item) => total + item.FinalPrice, 0);
 }
 
 renderCartContents();
