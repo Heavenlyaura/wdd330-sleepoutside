d127bb77644a949c1cb2ee546ccc86026cd529c3
soliverson <stacy.oliverson@gmail.com>
2024-07-03 20:21:14 -0600

    git

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,5 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
+const baseURL = import.meta.env.VITE_SERVER_URL;
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -11,6 +12,8 @@ export default class ProductData {
   constructor(category) {
     // this.category = category;
     // this.path = `../json/${this.category}.json`;
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
   }
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
@@ -18,6 +21,9 @@ export default class ProductData {
     return data.Result;
   }
   async findProductById(id) {
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
     const response = await fetch(baseURL + `product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
