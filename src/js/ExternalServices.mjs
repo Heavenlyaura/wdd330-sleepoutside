aa431fb0aaddd3e0a744df029664bb3e4d84809a
kmkryz <kmkryz@gmail.com>
2024-07-05 18:35:13 -0400

    submit the order

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,4 +1,30 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
+// const baseURL = import.meta.env.VITE_SERVER_URL;
+// function convertToJson(res) {
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Bad Response");
+//   }
+// }
+
+// export default class ExternalServices {
+//   constructor(category) {
+//     // this.category = category;
+//     // this.path = `../json/${this.category}.json`;
+//   }
+//   async getData(category) {
+//     const response = await fetch(baseURL + `products/search/${category}`);
+//     const data = await convertToJson(response);
+//     console.log(data);
+//     return data.Result;
+//   }
+//   async findProductById(id) {
+//     const response = await fetch(baseURL + `product/${id}`);
+//     const data = await convertToJson(response);
+//     return data.Result;
+//   }
+// }
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -7,20 +33,31 @@ function convertToJson(res) {
   }
 }
 
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
 export default class ExternalServices {
-  constructor(category) {
-    // this.category = category;
-    // this.path = `../json/${this.category}.json`;
-  }
   async getData(category) {
-    const response = await fetch(baseURL + `products/search/${category}`);
+    const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
-    console.log(data);
     return data.Result;
   }
+
   async findProductById(id) {
-    const response = await fetch(baseURL + `product/${id}`);
+    const response = await fetch(`${baseURL}product/${id}`);
     const data = await convertToJson(response);
-    return data.Result;
+    return data;
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    };
+    const response = await fetch(`${baseURL}checkout/`, options);
+    const data = await convertToJson(response);
+    return data;
   }
 }
