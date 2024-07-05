07b597fb2593e174a2404e66c4feb26e35e5b0d8
kmkryz <kmkryz@gmail.com>
2024-07-05 18:30:11 -0400

    ExternalServices changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
 
 const productList = document.querySelector(".product-list");
-let productData = new ProductData("tents");
-let productListing = new ProductList("tents", productData, productList);
+let externalServices = new ExternalServices("tents");
+let productListing = new ProductList("tents", externalServices, productList);
 
 productListing.init();
