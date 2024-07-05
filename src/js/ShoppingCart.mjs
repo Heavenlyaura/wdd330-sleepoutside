76228562352ceaf07143841403fa9537a231dd4b
heavenlyAura <goodnessokafor462@gmial.com>
2024-07-05 10:34:00 -0700

    "Updated cart HTML structure and CSS styles, and modified cart item template in JavaScript"

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -4,18 +4,13 @@ import { loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
 
 function cartItemTemplate(item) {
-  return `<li class="cart-card divider">
-    <span class="remove-item" data-id="${item.Id}">X</span>
-    <a href="#" class="cart-card__image">
-      <img src="${item.Image}" alt="${item.Name}" />
-    </a>
-    <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
+  return `
+    <p class="cart-card divider">
+      <span class="card__image"><img src="${item.Images.PrimarySmall}" alt="${item.Name}" /></span>
+      <span class="card__name">${item.NameWithoutBrand}</h2></span>
+      <span class="card__price">$${item.FinalPrice}</span>
+      <span class="remove-item" data-id="${item.Id}">X</span>
+    </p>`;
 }
 
 export default class ShoppingCart {
