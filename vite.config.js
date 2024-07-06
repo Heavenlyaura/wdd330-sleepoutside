dd6aa00b18c835d46fc6758d1d4c543facfc38e5
heavenlyAura <goodnessokafor462@gmial.com>
2024-07-06 12:56:37 -0700

    "Added 'success' entry to vite.config.js"

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         productListing: resolve(__dirname, "src/product-listing/index.html"), // Add this line
+        success: resolve(__dirname, "src/checkout/success.html")
       },
     },
   },
